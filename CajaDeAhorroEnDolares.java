0c631deb2de73cb72d4a00a3ee74f04d59c501e8
leansw <65741989+leansw@users.noreply.github.com>
2020-05-26 21:18:11 -0300

    Update CajaDeAhorroEnDolares.java

--- CajaDeAhorroEnDolares.java
+++ CajaDeAhorroEnDolares.java
@@ -4,20 +4,14 @@ import java.math.BigDecimal;
 
 public class CajaDeAhorroEnDolares extends Cuenta {
 
-	private BigDecimal impuestoPais;
-	private BigDecimal precioDolar;
-
 	public CajaDeAhorroEnDolares(Cliente cliente, String alias) {
 		super(cliente, alias);
-		impuestoPais = new BigDecimal(0.30);
-		precioDolar = new BigDecimal(68);
 	}
 
 	// montoIngresado es en pesos
 	@Override
 	public void ingresarEfectivo(BigDecimal montoIngresado) {
-		setSaldo(consultarSaldo().add(aplicarImpuestoPAIS(montoIngresado))
-				.divide(precioDolar));
+		setSaldo(consultarSaldo().add(montoIngresado));
 
 	}
 
@@ -36,7 +30,4 @@ public class CajaDeAhorroEnDolares extends Cuenta {
 		return consultarSaldo().compareTo(montoDescontado) < 0;
 	}
 
-	private BigDecimal aplicarImpuestoPAIS(BigDecimal montoIngresado) {
-		return montoIngresado.subtract(montoIngresado.multiply(impuestoPais));
-	}
 }
