bbe14c26c920d309fab2596dc71c12bb4739a511
leansw <65741989+leansw@users.noreply.github.com>
2020-05-28 23:34:23 -0300

    Update App.java

--- App.java
+++ App.java
@@ -1,14 +1,9 @@
 
 public class App {
 
-	public static int m2(int n) {
-		if(n <= 2) {
-			return 1;
-		}else {
-			return m2(n-1) + m2(n-2);
-		}
-	}
 	public static void main(String [] args) {
-		System.out.print(m2(5));
+		ATM atm = new ATM();
+		atm.leerTarjeta();
+		System.out.print(atm.imprimirTicket());
 	}
 }
