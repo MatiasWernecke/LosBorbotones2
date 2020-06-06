a4c87d386b30fb07170470e74c132850b88d3653
Matias Wernecke <63934637+wero4@users.noreply.github.com>
2020-06-06 13:02:42 -0300

    Update RetirarEfectivo.java

--- RetirarEfectivo.java
+++ RetirarEfectivo.java
@@ -1,4 +1,3 @@
-
 import java.math.BigDecimal;
 
 public class RetirarEfectivo extends Transaccion {
@@ -6,21 +5,21 @@ public class RetirarEfectivo extends Transaccion {
 	public RetirarEfectivo(Cuenta cuenta) {
 		super(cuenta);
 	}
-	
+
 	// saldo debe ser mayor o igual a monto
 	public void retirarEfectivo(BigDecimal montoRetirado) {
-			// A.compareTo(B): este metodo retorna -1 si A < B, 0 si A = B, 1 si
-			// A > B
-			if (haySaldo(montoRetirado)) {
-				super.setMonto(montoRetirado);
-				super.getCuenta().descontarEfectivo(montoRetirado);
-				super.generarMovimiento();
-				System.out.println("la extraccion se realizo con exito");
+		// A.compareTo(B): este metodo retorna -1 si A < B, 0 si A = B, 1 si
+		// A > B
+		if (haySaldo(montoRetirado)) {
+			super.setMonto(montoRetirado.negate());
+			super.getCuenta().descontarEfectivo(montoRetirado);
+			super.generarMovimiento();
+			System.out.println("la extraccion se realizo con exito");
 
-			} else {
-				System.err.println("Saldo insuficiente");
-			}
-		
+		} else {
+			throw new Error("Saldo Insuficiente");
+		}
+
 	}
 
 	private boolean haySaldo(BigDecimal montoRetirado) {
