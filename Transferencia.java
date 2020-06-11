b21743dc3e719cb2c64142edc6780566bb5c1a08
leansw <65741989+leansw@users.noreply.github.com>
2020-06-11 12:30:48 -0300

    Actualice las haySaldo que no tenia, y la condicion que pedira atm para imprimir ticket

--- Transferencia.java
+++ Transferencia.java
@@ -5,6 +5,8 @@ public class Transferencia extends Transaccion implements Reversible {
 	private Cuenta cuentaAtransferir;
 	private BigDecimal monto;
 	private Cuenta cuenta;
+	private boolean transferenciaRealizada = false;
+
 
 	public Transferencia(Cuenta cuenta) {
 		super(cuenta);
@@ -13,17 +15,33 @@ public class Transferencia extends Transaccion implements Reversible {
 
 	}
 	
-		public void transferencia(BigDecimal montoATransferir,
-			Cuenta cuentaATransferir) {
-		if (cuentaATransferir instanceof CajaDeAhorroEnPesos || cuentaATransferir instanceof CuentaCorriente) {
+	public void transferencia(BigDecimal montoATransferir, Cuenta cuentaATransferir) {
+		if (cuentaATransferir instanceof CajaDeAhorroEnPesos 
+				|| cuentaATransferir instanceof CuentaCorriente) {
+			
+			if(haySaldo(montoATransferir)) {
+			
+			transferenciaRealizada = true;
+			
 			super.setMonto(montoATransferir);
+			
 			setMonto(montoATransferir);
+			
 			setCuenta(cuentaATransferir);
+			
 			this.cuenta.descontarEfectivo(montoATransferir);
+			
 			cuentaATransferir.ingresarEfectivo(montoATransferir);
+			
 			super.generarMovimiento();
-
-		} 
+			
+			} else {
+				System.err.println("Saldo insuficiente para transferir");
+			}
+		
+		} else {
+			System.err.println("No se puede transferir a una caja de ahorro en dolares");
+		}
 	}
 
 	public BigDecimal getMonto() {
@@ -45,10 +63,15 @@ public class Transferencia extends Transaccion implements Reversible {
 	
 	@Override
 	public void reversible() {
-
 		super.getCuenta().ingresarEfectivo(this.monto);
 		this.cuentaAtransferir.descontarEfectivo(this.monto);
-
 	}
-
+	
+	private boolean haySaldo(BigDecimal montoATransferir) {
+		return this.cuenta.getSaldo().compareTo(montoATransferir) >= 0;
+	}
+	
+	public boolean getAceptacionTransferencia() {
+		return transferenciaRealizada;
+	}
 }
